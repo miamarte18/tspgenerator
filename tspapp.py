3914570e6593913f0c3f4c213437436316cc0106
miamarte18 <97921806+miamarte18@users.noreply.github.com>
2023-06-20 22:29:22 -0400

    Update tspapp.py

--- tspapp.py
+++ tspapp.py
@@ -1,234 +1,121 @@
-#TSP project
-#Purpose: From the starting point of the path, we wanna find the shorterst path of the distance.
-#Last day edited: 6/20/2023
-#Autors: Mia Marte, Sofia Torres, Stephanie Saenz
 
-import random
-from math import sqrt
-import streamlit as st 
-
-import matplotlib.patches as patches
-import matplotlib.pyplot as plt
+import streamlit as st
 import numpy as np
-from matplotlib.path import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import random
+import itertools
+from gurobipy import *
+plt.rcParams['savefig.pad_inches'] = 0
+st.title('Travelling salesman problem')
+
+# Callback - use lazy constraints to eliminate sub-tours
+
+def subtourelim(model, where):
+    if where == GRB.Callback.MIPSOL:
+        # make a list of edges selected in the solution
+        vals = model.cbGetSolution(model._vars)
+        selected = tuplelist((i,j) for i,j in model._vars.keys() if vals[i,j] > 0.5)
+        # find the shortest cycle in the selected edge list
+        tour,tours = subtour(selected)
  
+        if len(tour) < n:
+            model._subtours += 1
+            # add subtour elimination constraint for every pair of cities in tour
+            model.cbLazy(quicksum(model._vars[i,j]
+                                  for i,j in itertools.combinations(tour, 2))
+                         <= len(tour)-1)
+               #st.write(tour)
+        current_length = round(model.cbGet(GRB.Callback.MIPSOL_OBJ))
+        best = round(model.cbGet(GRB.Callback.MIPSOL_OBJBST))
+        bound = max(0,round(model.cbGet(GRB.Callback.MIPSOL_OBJBND)))
+        model._summary.markdown("**Sub tour elimination constraints** {:d} **Lower bound** {:d}km  \n**Current Solution**  {:d}km  - {:d} subtour(s)".format(model._subtours,bound,current_length,len(tours)))
+        #TODO update bound in other callback. Structure output
+        plt.plot([x[0] for x in points], [x[1] for x in points], 'o')
+        #print("total tours " + str(sum(len(t) for t in tours)))
+        for tour in tours:
+            tour.append(tour[0])
+            points_tour = [points[i] for i in tour]
+            plt.plot([x[0] for x in points_tour], [x[1] for x in points_tour], '-')
+        plt.axis([0, 105, 0, 105])
+        plt.xlabel("km")
+        plt.ylabel("km")
+        model._plot.pyplot()
+        
 
-cityL = []
-demographic = []
+# Given a tuplelist of edges, find the shortest subtour
 
-with st.container():
-    st.subheader("Project 2 by Sofia Torres, Stephanie Seanz, and Mia Marte 👋")
-    st.title('Travelling salesman problem')
-
-
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def distance(self, city):
-        xdis = abs(self.x - city.x)
-        ydis = abs(self.y - city.y)
-        dis = sqrt((xdis**2) + (ydis**2))
-        return dis
-
-    def __repr__(self):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
-
-class Fitness:
-    def __init__(self, route):
-        self.route = route
-        self.dis = 0
-        self.fit = 0
-
-    def routeDistance(self):
-        if self.dis == 0:
-            pathDis = 0
-            # looping through route to calculate distance
-            for i in range(0, len(self.route)):
-                fromCity = self.route[i]
-                toCity = None
-                if i + 1 < len(self.route):
-                    # getting next city in route
-                    toCity = self.route[i + 1]
-                else:
-                    # returning to start city
-                    toCity = self.route[0]
-                pathDis += fromCity.distance(toCity)
-            self.dis = pathDis
-        return self.dis
-
-    def fitnessScore(self):
-        if self.fit == 0:
-            self.fit = 1/float(self.routeDistance())
-        return self.fit
-
-def plotCities():
-    x = [cities.x for cities in cityL]
-    y = [cities.y for cities in cityL]
-    plt.scatter(x, y)
-    plt.plot(x, y)
-    plt.xlabel("X values")
-    plt.ylabel("Y values")
-    plt.show()
+def subtour(edges):
+    unvisited = list(range(n))
+    cycle = range(n+1) # initial length has 1 more city
+    cycles = []
+    while unvisited: # true if list is non-empty
+        thiscycle = []
+        neighbors = unvisited
+        while neighbors:
+            current = neighbors[0]
+            thiscycle.append(current)
+            unvisited.remove(current)
+            neighbors = [j for i,j in edges.select(current,'*') if j in unvisited]
+        if len(cycle) > len(thiscycle):
+            cycle = thiscycle
+        cycles.append(thiscycle)
+    return (cycle,cycles)
 
 
-def initValues(size):
-    # Initial City List
-    for i in range(0, 25):
-        cityL.append(City(x=int(random.random()*200),
-                        y=int(random.random()*200)))
+n = st.slider('How many destinations to generate?', 5, 200, 5)
+# Create n random points
+points = [(random.randint(0,100),random.randint(0,100)) for i in range(n)]
 
-    # Initial demographic
-    for i in range(0, size):
-        demographic.append(createRoute())
+# Dictionary of Euclidean distance between each pair of points
 
-def createRoute():
-    route = random.sample(cityL, len(cityL))
-    return route
+dist = {(i,j) :
+    math.sqrt(sum((points[i][k]-points[j][k])**2 for k in range(2)))
+    for i in range(n) for j in range(i)}
 
+m = Model()
+m._subtours = 0
+m._summary = st.empty()
+m._plot = st.empty()
+m._points = points
+# Create variables
 
-def rankRoutes():
-    results = {}
+vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
+for i,j in vars.keys():
+    vars[j,i] = vars[i,j] # edge in opposite direction
 
-    for i in range(0, len(demographic)):
-        results[i] = Fitness(route=demographic[i]).fitnessScore()
+# Add degree-2 constraint
 
-    return sorted(results.items(), key=lambda kv: kv[1], reverse=True)
+m.addConstrs(vars.sum(i,'*') == 2 for i in range(n))
 
+# Optimize model
 
-def slection(rankedList, size):
-    selectionResults = []
+m._vars = vars
+m.Params.lazyConstraints = 1
+m.optimize(subtourelim)
 
-    for i in range(0, size):
-        selectionResults.append(rankedList[i][0])
-    for i in range(0, len(rankedList) - size):
-        player_1 = random.randint(0, len(rankedList)-1)
-        player_2 = random.randint(0, len(rankedList)-1)
-        if rankedList[player_1][1] >= rankedList[player_2][1]:
-            selectionResults.append(rankedList[player_1][0])
-        else:
-            selectionResults.append(rankedList[player_2][0])
+vals = m.getAttr('x', vars)
+selected = tuplelist((i,j) for i,j in vals.keys() if vals[i,j] > 0.5)
 
-    return selectionResults
+tour,tours = subtour(selected)
+assert len(tour) == n
+tour.append(tour[0])
+points_tour = [points[i] for i in tour]
 
-def matingPool(demographic, selectionResults):
-    pool = []
+current_length = round(m.objVal)
+best = round(m.objVal)
+bound = max(0,round(m.objVal))
+m._summary.markdown("**Sub tour elimination constraints** {:d} **Lower bound** {:d}km  \n**Current Solution**  {:d}km  - {:d} subtour(s)".format(m._subtours,bound,current_length,len(tours)))
 
-    for i in range(0, len(selectionResults)):
-        index = selectionResults[i]
-        pool.append(demographic[index])
-    return pool
+plt.plot([x[0] for x in points_tour], [x[1] for x in points_tour], '-o')
+plt.axis([0, 105, 0, 105])
+plt.xlabel("km")
+plt.ylabel("km")
+m._plot.pyplot()
 
-
-def breed(p1, p2):
-    child = []
-    childP1 = []
-    childP2 = []
-
-    # ordered crossover
-    geneA = int(random.random() * len(p1))
-    geneB = int(random.random() * len(p1))
-
-    startGene = min(geneA, geneB)
-    endGene = max(geneA, geneB)
-
-    for i in range(startGene, endGene):
-        childP1.append(p1[i])
-
-    childP2 = [gene for gene in p2 if gene not in childP1]
-
-    child = childP1 + childP2
-    return child
-
-def breedPop(pool, size):
-    children = []
-    total = len(pool) - size
-    sameplePool = random.sample(pool, len(pool))
-
-    # using elitism
-    for i in range(0, size):
-        children.append(pool[i])
-
-    # breed for rest of pool
-    for i in range(0, total):
-        child = breed(sameplePool[i], sameplePool[(len(pool) - i) - 1])
-        children.append(child)
-    return children
-
-
-def mutate(individual, mutationRate):
-    for swapped in range(len(individual)):
-        if(random.random() < mutationRate):
-            swapWith = int(random.random() * len(individual))
-
-            city1 = individual[swapped]
-            city2 = individual[swapWith]
-
-            individual[swapped] = city2
-            individual[swapWith] = city1
-    return individual
-
-def mutatedemographic(children, mutationRate):
-    mutatedPop = []
-
-    for i in range(0, len(children)):
-        mutated = mutate(children[i], mutationRate)
-        mutatedPop.append(mutated)
-    return mutatedPop
-
-
-def nextGeneration(currentGen, eliteSize, mutationRate):
-    rankedList = rankRoutes()
-    selectionResults = slection(rankedList, eliteSize)
-    matingpool = matingPool(currentGen, selectionResults)
-    children = breedPop(matingpool, eliteSize)
-    nextGeneration = mutatedemographic(children, mutationRate)
-    return nextGeneration
-def geneAlgo(popSize, eliteSize, mutationRate, generations):
-    global demographic
-    initValues(popSize)
-    progress = []
-    print("The initial distance is: " + str(1 / rankRoutes()[0][1]))
-    progress.append(1 / rankRoutes()[0][1])
-
-    for i in range(0, generations):
-        demographic = nextGeneration(demographic, eliteSize, mutationRate)
-        progress.append(1 / rankRoutes()[0][1])
-
-    print("The final distance is: " + str(1 / rankRoutes()[0][1]))
-    bestRouteIndex = rankRoutes()[0][0]
-    bestRoute = demographic[bestRouteIndex]
-    fig, axs = plt.subplots(2)
-   
-
-    axs[0].plot(progress)
-    axs[0].set_title("Progress Graph")
-    #axs[1].set_ylabel('\nDistance\n')
-    
-
-    x = [cities.x for cities in bestRoute]
-    x.append(bestRoute[0].x)
-    y = [cities.y for cities in bestRoute]
-    y.append(bestRoute[0].y)
- 
-    axs[1].set_title("Final Path Graph")
-    axs[1].scatter(x, y)
-    axs[1].plot(x, y)
-    axs[1].set_xlabel("X values")
-    axs[1].set_ylabel("Y values")
-
-    plt.tight_layout()
-    plt.show()
-    return bestRoute
-if __name__ == '__main__':
-    print("Start")
-
-    initValues(size=25)
-    plotCities()
-    bestRoute = geneAlgo(popSize=100, eliteSize=20,
-                         mutationRate=0.01, generations=500)
-    
-    cityL = []
-    for i in range (0, 25):
-        cityL.append(City(x=int(random.random() * 200), y=int(random.random() *200)))
+st.write('')
+#st.write('Optimal tour: %s' % str(tour))
+st.write('Optimal cost: {:0.1f}km'.format(m.objVal))
+st.write('Running time to optimize: {:0.1f}s'.format(m.Runtime))
+st.write('Sub tour constraint added: {:d}'.format(m._subtours)) 
